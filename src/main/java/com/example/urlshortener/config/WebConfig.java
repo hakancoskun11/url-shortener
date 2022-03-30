867c3f7574ca1db370365ca219f86de6f58121e1
hakancoskun11 <hakancoskun360@gmail.com>
2022-03-30 16:26:05 +0300

    message added

--- src/main/java/com/example/urlshortener/config/WebConfig.java
+++ src/main/java/com/example/urlshortener/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+//request isteklerine izin verildi.
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
